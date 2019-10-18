3622b762c784a64b6fd86ab621490098e9245a26
Lucas Kjær Helgogaard <lkhelgo@gmail.com>
2019-10-18 11:54:45 +0200

    Login with Email and Password works

--- app/src/main/java/com/project/cookex/login_and_register/LoginActivity.java
+++ app/src/main/java/com/project/cookex/login_and_register/LoginActivity.java
@@ -12,12 +12,14 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.project.cookex.HomeActivity;
 import com.project.cookex.R;
 
 /**
@@ -58,6 +60,9 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // Redirect inside app etc
+                String email = mEmailField.getText().toString().trim();
+                String password = mPasswordField.getText().toString().trim();
+                signIn(email, password);
             }
         });
 
@@ -85,10 +90,18 @@ public class LoginActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
 
                             // Sign in success, update UI with the signed-in user's information
+                            System.out.println("Signed In");
+                            Toast.makeText(LoginActivity.this, "Sign in Successful!", Toast.LENGTH_SHORT).show();
+
+                            Intent loginSuccess = new Intent(LoginActivity.this, HomeActivity.class);
+                            loginSuccess.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK + Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                            startActivity(loginSuccess);
 
                         } else {
 
                             // If sign in fails, display a message to the user.
+                            System.out.println("Unable to sign in");
+                            Toast.makeText(getApplicationContext(), "Login Failed.", Toast.LENGTH_SHORT).show();
 
                         }
 
